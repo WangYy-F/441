295a6866fc290bce6c69418b270b494a29f2fc6b
Wang Yangyang <wangyangyang@sjtu.edu.cn>
2023-08-03 23:39:52 -0400

    debug lab3

--- lab3/kotlinJpCChatter/app/src/main/java/cn/edu/sjtu/wangyangyang/kotlinJpCChatter/ChattListRow.kt
+++ lab3/kotlinJpCChatter/app/src/main/java/cn/edu/sjtu/wangyangyang/kotlinJpCChatter/ChattListRow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChattListRow(index: Int, chatt: Chatt) {
     Column(modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp)
